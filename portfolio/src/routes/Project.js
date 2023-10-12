a092175eddcc9db742513c075851de660b6a9f0d
Minakshivyavahare <minakshi.tophat@gmail.com>
2023-10-12 12:39:18 +0530

    adding contact and about us page

--- portfolio/src/routes/Project.js
+++ portfolio/src/routes/Project.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
+import PricingCard from '../components/PricingCard'
 
 const Project = () => {
   return (
     <div>
        <Navbar/>
+       <HeroImg2 heading="PROJECTS." text="Some of my most recent works"/>
+       <PricingCard />
        <Footer/>
     </div>
   )
