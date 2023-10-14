fb94642a03ce89cc6337d7e715786fc62740df93
Minakshivyavahare <minakshi.tophat@gmail.com>
2023-10-14 16:36:25 +0530

    Dynamic Card component

---
+++ portfolio/src/components/WorkCardData.js
@@ -0,0 +1,30 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+
+
+const ProjectCardData = [
+    {
+        imgsrc:pro1,
+        title:"Ecommerce Project Design",
+        text:"Thriving Facebook community and top-ranked podcast,Amy inspires",
+        view:"https://www.w3schools.com/js/",
+
+    },
+    {
+        imgsrc:pro2,
+        title:"LMS/Course Website Design",
+        text:"Thriving Facebook community and top-ranked podcast,Amy inspires",
+        view:"https://www.w3schools.com/js/",
+
+    },
+    {
+        imgsrc:pro3,
+        title:"Rymo Ecommerce Design",
+        text:"Thriving Facebook community and top-ranked podcast,Amy inspires",
+        view:"https://www.w3schools.com/js/",
+
+    }
+]
+export default ProjectCardData;
